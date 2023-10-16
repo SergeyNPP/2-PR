a5fe63d8e7c727aecb314db142fc8bec17f0d182
SBudanov <budanoff.2011@yandex.ru>
2023-10-16 10:56:21 +0300

    Initial commmit PR2

---
+++ Zadacha23/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+System.Console.WriteLine("Выходной?");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a>0 & a<8)
+{
+    if(a>0 & a<6)
+    {
+    System.Console.Write(" -> нет");
+    }
+    else
+    {
+    System.Console.Write(" -> да");       
+    }
+}
+else
+{
+    System.Console.Write("Нет такого дня");
+}
